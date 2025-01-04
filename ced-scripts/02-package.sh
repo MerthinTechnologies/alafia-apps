08ef2b42ad1047504bde753f00f9126a47cb2f3f
gprotel <luis@merthin.com>
2025-01-04 16:33:38 +0000

    adding desktop shortcut removal and custom incons handling

--- ced-scripts/02-package.sh
+++ ced-scripts/02-package.sh
@@ -5,6 +5,8 @@ PACKAGE_NAME="alafia-$CED_SUBSYSTEM_ID"
 PACKAGE_CUSTOM_NAME="$CED_SUBSYSTEM_NAME"
 PACKAGE_VERSION="$CED_VERSION_ID"
 EXEC_FILE="$CED_SUBSYSTEM_CONFIG_EXEC_FILE"
+ICON_URL="$CED_SUBSYSTEM_CONFIG_ICON_URL"
+
 
 MAINTAINER="Alafia Support <support@alafia.ai>"
 ARCHITECTURES=(amd64 arm64)
@@ -24,12 +26,16 @@ TEMP_DIR=$(mktemp -d)
 mv $BASE_DIR/* $TEMP_DIR/
 
 WORKDIR="${BASE_DIR}/${PACKAGE_NAME}-${PACKAGE_VERSION}"
-mkdir -p "$WORKDIR/debian" "$WORKDIR/opt/$PACKAGE_NAME"
+mkdir -p "$WORKDIR/debian/icons" "$WORKDIR/opt/$PACKAGE_NAME"
+ICON_FILE="$WORKDIR/debian/icons/$PACKAGE_NAME.png"
 
 cd "$WORKDIR"
 
 # Copy precompiled binaries and libraries
 cp -fr "$TEMP_DIR/"* "$WORKDIR/opt/$PACKAGE_NAME/"
+
+# Getting the icon
+curl -o "$ICON_FILE" "$ICON_URL"
 
 # Create debian/control
 cat <<EOF > debian/control
@@ -74,6 +80,7 @@ override_dh_install:
 	mkdir -p debian/$PACKAGE_NAME/usr/share/applications
 	cp debian/$PACKAGE_NAME.desktop debian/$PACKAGE_NAME/usr/share/applications/
 	install -Dm755 debian/postinst debian/$PACKAGE_NAME/DEBIAN/postinst
+	install -Dm755 debian/postrm debian/$PACKAGE_NAME/DEBIAN/postrm
 
 EOF
 chmod +x debian/rules
@@ -82,6 +89,7 @@ chmod +x debian/rules
 cat <<EOF > debian/install
 opt/$PACKAGE_NAME/* /opt/$PACKAGE_NAME/
 debian/$PACKAGE_NAME.desktop /usr/share/applications/
+debian/icons/$PACKAGE_NAME.png /usr/share/icons/hicolor/128x128/apps/
 EOF
 
 # Create debian/$PACKAGE_NAME.desktop
@@ -91,7 +99,7 @@ Version=$PACKAGE_VERSION
 Name=$PACKAGE_CUSTOM_NAME
 Comment=Run the Alafia $PACKAGE_CUSTOM_NAME application
 Exec=/opt/$PACKAGE_NAME/$EXEC_FILE
-Icon=application-default-icon
+Icon=$PACKAGE_NAME
 Terminal=false
 Type=Application
 Categories=Utility;
@@ -105,15 +113,34 @@ set -e
 USER_NAME=\$(logname)
 USER_HOME=\$(getent passwd \$USER_NAME | cut -d: -f6)
 
+if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    gtk-update-icon-cache -q /usr/share/icons/hicolor || true
+fi
+
 if [ -d "\$USER_HOME/Desktop" ]; then
     sudo -u \$USER_NAME cp /usr/share/applications/$PACKAGE_NAME.desktop "\$USER_HOME/Desktop/"
     sudo -u \$USER_NAME chmod +x "\$USER_HOME/Desktop/$PACKAGE_NAME.desktop"
     sudo -u \$USER_NAME env XDG_RUNTIME_DIR=/run/user/\$(id -u \$USER_NAME) gio set "\$USER_HOME/Desktop/$PACKAGE_NAME.desktop" metadata::trusted true || true
-
 fi
+
 EOF
 
 chmod +x debian/postinst
+
+# Create debian/postrm
+cat <<EOF > debian/postrm
+#!/bin/bash
+set -e
+
+USER_NAME=\$(logname)
+USER_HOME=\$(getent passwd \$USER_NAME | cut -d: -f6)
+
+if [ -d "\$USER_HOME/Desktop" ]; then
+    rm -f "\$USER_HOME/Desktop/$PACKAGE_NAME.desktop"
+fi
+EOF
+
+chmod +x debian/postrm
 
 # Build the Package for Each Architecture
 for ARCH in "${ARCHITECTURES[@]}"; do
